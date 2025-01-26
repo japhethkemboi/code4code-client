16b8c4c99e003ac53ae58afa47c6b4fe78d2783e
japhethkemboi <hazardovic1@gmail.com>
2025-01-26 15:14:37 +0300

    ffg

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,16 @@
 "use client";
+import { useEffect, useState } from "react";
 import DOMPurify from "dompurify";
 import { fetchConfig } from "../fetchConfig";
-import { useEffect, useState } from "react";
 import { toast } from "c4cui";
 import Link from "next/link";
 
 export default function AboutUs() {
   const [about, setAbout] = useState("");
+  const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
+    setIsClient(true);
     fetchAbout();
   }, []);
 
@@ -16,11 +18,15 @@ export default function AboutUs() {
     const res = await fetchConfig(`/about/`);
 
     if (res.data) {
-      setAbout(about);
+      setAbout(res.data);
     } else {
-      toast.error(res.error || "Could'nt fetch info.");
+      toast.error(res.error || "Couldn't fetch info.");
     }
   };
+
+  if (!isClient) {
+    return null;
+  }
 
   return (
     <div className="flex flex-col h-full gap-8 bg-[var(--background-color)] text-[var(--text-color)]">
@@ -28,13 +34,17 @@ export default function AboutUs() {
         <h1 className="text-4xl">About Us</h1>
       </div>
       <div className="flex flex-col gap-4 p-4">
-        <div className="md:text-lg text-black p-4" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(about) }} />
+        <div
+          className="md:text-lg text-black p-4"
+          dangerouslySetInnerHTML={{
+            __html: DOMPurify.sanitize(about),
+          }}
+        />
         <p>
           Contact us at{" "}
           <Link href="mailto:contact@code4code.dev" className="text-[var(--hover-color)] underline">
             contact@code4code.dev
           </Link>
-          .
         </p>
       </div>
     </div>

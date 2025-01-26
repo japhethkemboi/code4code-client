409d2e6d2fd7412688bc65cf8cdeb3b3b43d5e57
japhethkemboi <hazardovic1@gmail.com>
2025-01-27 01:02:21 +0300

    ddf

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -3,34 +3,30 @@ import { fetchConfig } from "@/app/fetchConfig";
 import { cookies } from "next/headers";
 
 export async function POST(request: NextRequest) {
-  try {
-    const cookieStore = await cookies();
-    const access = cookieStore.get("access");
+  const cookieStore = await cookies();
+  const access = cookieStore.get("access");
 
-    if (access) {
-      const res = await fetchConfig("/user/logout/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${access.value}`,
-        },
-      });
+  if (access) {
+    const res = await fetchConfig("/user/logout/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${access.value}`,
+      },
+    });
 
-      if (res.error && res.status !== 401) {
-        return NextResponse.json({ error: res.error || "Logout failed. Please try again." }, { status: 400 });
-      } else {
-        cookieStore.delete("access");
-        cookieStore.delete("refresh");
-
-        return NextResponse.json({ message: "Logged out successfully" }, { status: 200 });
-      }
+    if (res.error && res.status !== 401) {
+      return NextResponse.json({ error: res.error || "Logout failed. Please try again." }, { status: 400 });
     } else {
       cookieStore.delete("access");
       cookieStore.delete("refresh");
 
       return NextResponse.json({ message: "Logged out successfully" }, { status: 200 });
     }
-  } catch (error) {
-    return NextResponse.json({ error: "Internal server error. Please try again later." }, { status: 500 });
+  } else {
+    cookieStore.delete("access");
+    cookieStore.delete("refresh");
+
+    return NextResponse.json({ message: "Logged out successfully" }, { status: 200 });
   }
 }
--- app/api/refresh/route.ts
+++ app/api/refresh/route.ts
@@ -17,49 +17,44 @@ export async function POST(request: NextRequest) {
     return NextResponse.json({ error: "Please wait before trying to refresh again." }, { status: 429 });
   }
 
-  try {
-    const res = await fetchConfig("/user/token/refresh/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ refresh: refreshToken.value }),
+  const res = await fetchConfig("/user/token/refresh/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ refresh: refreshToken.value }),
+  });
+
+  if (res.data) {
+    const { access, refresh } = res.data;
+
+    const response = NextResponse.json({ ok: true });
+    response.cookies.set("access", access, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 15 * 60,
+      path: "/",
+      sameSite: "strict",
     });
 
-    if (res.data) {
-      const { access, refresh } = res.data;
+    response.cookies.set("refresh", refresh, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 24 * 60 * 60,
+      path: "/",
+      sameSite: "strict",
+    });
 
-      const response = NextResponse.json({ ok: true });
-      response.cookies.set("access", access, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        maxAge: 15 * 60,
-        path: "/",
-        sameSite: "strict",
-      });
+    response.cookies.set("last_refresh_time", currentTime.toString(), {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 24 * 60 * 60,
+      path: "/",
+      sameSite: "strict",
+    });
 
-      response.cookies.set("refresh", refresh, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        maxAge: 24 * 60 * 60,
-        path: "/",
-        sameSite: "strict",
-      });
-
-      response.cookies.set("last_refresh_time", currentTime.toString(), {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        maxAge: 24 * 60 * 60,
-        path: "/",
-        sameSite: "strict",
-      });
-
-      return response;
-    } else {
-      return NextResponse.json({ error: "Failed to refresh token." }, { status: 401 });
-    }
-  } catch (error) {
-    console.error("Error refreshing token", error);
-    return NextResponse.json({ error: "Error refreshing token." }, { status: 500 });
+    return response;
+  } else {
+    return NextResponse.json({ error: "Failed to refresh token." }, { status: 401 });
   }
 }

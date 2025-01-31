f979f54810c577f9781b8988f531e18750db46d0
japhethkemboi <hazardovic1@gmail.com>
2025-01-31 14:15:15 +0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Header } from "./components/header";
 import { Footer } from "./components/footer";
+import { ModalProvider } from "c4cui";
 
 export const metadata: Metadata = {
   title: "CODE4CODE",
@@ -22,9 +23,11 @@ export default function RootLayout({
       <body
         className={`antialiased w-screen h-screen flex flex-col overflow-hidden overflow-y-auto bg-[var(--background-color)] text-[var(--text-color)]`}
       >
-        <Header />
-        {children}
-        <Footer />
+        <ModalProvider>
+          <Header />
+          {children}
+          <Footer />
+        </ModalProvider>
       </body>
     </html>
   );

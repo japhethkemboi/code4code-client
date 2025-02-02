2f8ebde07c48e65e0e9040595d9a60b15e2943a1
japhethkemboi <hazardovic1@gmail.com>
2025-02-02 08:44:42 +0300

    config

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,27 @@ const nextConfig: NextConfig = {
     ],
     formats: ["image/webp"],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://*.code4code.dev",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
